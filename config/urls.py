58a463190dfc6d9e270a35051f917e3148a03df6
Radosław Ganczarek <radoslaw@ganczarek.in>
2023-12-10 11:48:23 +0100

    Home site

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("", include("uebusaito.home.urls", namespace="home")),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # Your stuff: custom urls includes go here
--- tests/uebusaito/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def _media_storage(settings, tmpdir):
-    settings.MEDIA_ROOT = tmpdir.strpath
