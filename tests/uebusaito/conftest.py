6db6b068ccb9fe65be2ef148c85741c061dde737
Radosław Ganczarek <radoslaw@ganczarek.in>
2023-12-10 11:05:43 +0100

    Remove allauth and docs

--- tests/uebusaito/conftest.py
+++ tests/uebusaito/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 
-from tests.uebusaito.users.factories import UserFactory
-from uebusaito.users.models import User
-
 
 @pytest.fixture(autouse=True)
 def _media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture()
-def user(db) -> User:
-    return UserFactory()
